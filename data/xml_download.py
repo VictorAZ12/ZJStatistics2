5bc23a0a6f841834e0e8c56510e8c40a154eb931
YanchenZhao233 <819413991zhao@gmail.com>
2023-07-12 02:59:25 +0800

    Finish download script for anni1

---
+++ data/xml_download.py
@@ -0,0 +1,32 @@
+import asyncio
+import aiohttp
+import urllib.parse
+
+async def download_file(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            # Modify the filename as per your requirement
+            filename = url.split('/')[-1]
+            decoded_filename = urllib.parse.unquote(filename)
+            with open("./xml_files/"+decoded_filename, 'wb') as f:
+                while True:
+                    chunk = await response.content.read(1024)
+                    if not chunk:
+                        break
+                    f.write(chunk)
+            print(f"Downloaded: {decoded_filename}")
+
+async def main():
+    with open("./xmls_anni1.txt", "r") as f:
+        urls = [url.rstrip('\n') for url in f.readlines()][:3]
+    print(urls)
+    
+    tasks = []
+    for url in urls:
+        task = asyncio.create_task(download_file(url))
+        tasks.append(task)
+    
+    await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- data/xml_link_extract.py
+++ data/xml_link_extract.py
@@ -1,14 +1,21 @@
 import re
 files = []
-xmls = []
 with open("./anni1_xml.html", "r", encoding="UTF-8") as f:
-    files.append(f.read())
+    anni1= f.read()
 with open("./anni2_xml.html", "r", encoding="UTF-8") as f:
-    files.append(f.read())
+    anni2= f.read()
 # extract xmls and output
-for file in files:
-    xmls.extend(re.findall("list-item hope-stack hope-c-dhzjXW hope-c-PJLV hope-c-PJLV-ikoJJtX-css\" href=\"https:.*?\.xml",  file))
+header = "https://d.asoul.net.cn/0:/ASOUL-REC-%E4%B8%80%E5%91%A8%E5%B9%B4/XML%E5%BC%B9%E5%B9%95%E6%96%87%E4%BB%B6/"
+xmls= re.findall("list-item hope-stack hope-c-dhzjXW hope-c-PJLV hope-c-PJLV-ikoJJtX-css\" href=\"https:.*?\.xml",  anni1)
 for i, xml in enumerate(xmls):
-    xmls[i] = re.findall("https:.*?\.xml", xml)[0]
-with open("xmls.txt","w") as f:
+    xmls[i] = header + re.findall("https:.*?\.xml", xml)[0].split("/")[-1]
+with open("xmls_anni1.txt","w") as f:
     f.write("\n".join(xmls))
+
+xmls= re.findall("list-item hope-stack hope-c-dhzjXW hope-c-PJLV hope-c-PJLV-ikoJJtX-css\" href=\"https:.*?\.xml",  anni2)
+for i, xml in enumerate(xmls):
+    xmls[i] = header + re.findall("https:.*?\.xml", xml)[0]
+with open("xmls_anni2.txt","w") as f:
+    f.write("\n".join(xmls))
+
+    
